5c49e244a804917640faf0e15099683a30fc999d
robert <rdbruce>
2024-08-14 14:49:45 -0700

    ship project init commit

---
+++ ship/build/CMakeFiles/bobbyvsg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bobbyvsg.dir/src/main.cpp.o"
+  "CMakeFiles/bobbyvsg.dir/src/main.cpp.o.d"
+  "bobbyvsg"
+  "bobbyvsg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bobbyvsg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ship/build/CMakeFiles/bobbyvsg.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bobbyvsg.
+# This may be replaced when dependencies are built.
